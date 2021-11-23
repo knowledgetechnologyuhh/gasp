5a70ff5631c865aaaecb504ea907a816699f37b4
Fares Abawi <fabawi@users.noreply.github.com>
2021-11-23 23:16:57 +0100

    Update download_dataset.sh

--- datasets/processed/Grouped_frames/diem/download_dataset.sh
+++ datasets/processed/Grouped_frames/diem/download_dataset.sh
@@ -2,7 +2,7 @@
 
 wget -O diem.tar.gz https://www2.informatik.uni-hamburg.de/WTM/corpora/GASP/datasets/processed/Grouped_frames/diem.tar.gz
 
+tar -xf diem.tar.gz
 mv diem/* ./
 rm -rf diem/
-tar -xf diem.tar.gz
 rm diem.tar.gz
