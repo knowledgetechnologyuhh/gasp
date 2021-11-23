06b659e0ab82bfd539d248e02a6e6e51efee58c2
Fares Abawi <fabawi@users.noreply.github.com>
2021-11-23 23:16:36 +0100

    Update download_dataset.sh

--- datasets/processed/Grouped_frames/coutrot1/download_dataset.sh
+++ datasets/processed/Grouped_frames/coutrot1/download_dataset.sh
@@ -2,7 +2,7 @@
 
 wget -O coutrot1.tar.gz https://www2.informatik.uni-hamburg.de/WTM/corpora/GASP/datasets/processed/Grouped_frames/coutrot1.tar.gz
 
+tar -xf coutrot1.tar.gz
 mv coutrot1/* ./
 rm -rf coutrot1/
-tar -xf coutrot1.tar.gz
 rm coutrot1.tar.gz
