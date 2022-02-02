89278b5f0d2ceea3eaee895ab56563831ae6bfb7
Fares Abawi <fabawi@users.noreply.github.com>
2022-02-02 11:40:57 +0100

    Update download_dataset.sh

--- datasets/ave/database2/download_dataset.sh
+++ datasets/ave/database2/download_dataset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./megadown 'https://mega.nz/#!58dARTDR!AII7nCEktkeMqbZ2XqXBFyDsAeMKqBf9reXR-MHPuKk'
+./megadown.sh 'https://mega.nz/#!58dARTDR!AII7nCEktkeMqbZ2XqXBFyDsAeMKqBf9reXR-MHPuKk'
 wget -O coutrot_database2.mat "http://antoinecoutrot.magix.net/public/assets/coutrot_database2.mat"
 7za x "ERB4_Stimuli.zip"
 rm "ERB4_Stimuli.zip"
